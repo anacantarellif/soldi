f7303ff03eed7e9b730753d3b45714ddfaefd779
ana <acantarellif@gmail.com>
2024-04-30 11:53:51 -0300

    delete

--- front/nivel1.js
+++
@@ -1,6 +0,0 @@
-const propgresso = document.querySelector(".barra div")
-const input = document.querySelector("input")
-
-const alterarProgresso = () => {
-    propgresso.setAttribute("style", "width" + input.value + "%")
-}
