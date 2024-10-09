d1beaf09fd67481068afe1ef8312cea350a159d3
ana <acantarellif@gmail.com>
2024-10-09 11:44:41 -0300

    swagger criado

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,9 +2,57 @@ const express = require('express');
 const router = express.Router();
 const{storeUser, storeID, getDados} = require('../controller/userController')
 
-router.post('/user/create', storeUser);
-router.put('/user/:id', storeID);
-router.post('/user/dados/', getDados);
 // router.get('/pontos/:id', getPontos)
 
 module.exports = router;
+
+/** 
+@swagger
+*     /user/create:
+*     post:
+*         summary: Cadastra um usuário
+*     responses:
+*     200:
+*         description: Sucesso
+*         content:
+*             application/json:
+*                schema:
+*                 type: array
+*                 items:
+*                  type: object
+*/
+router.post('/user/create', storeUser);
+
+/** 
+@swagger
+*     /user/:id:
+*     put:
+*         summary: Atualiza os pontos de acordo com a avanço do usuário
+*     responses:
+*     200:
+*         description: Sucesso
+*         content:
+*             application/json:
+*                schema:
+*                 type: array
+*                 items:
+*                  type: object
+*/
+router.put('/user/:id', storeID);
+
+/** 
+@swagger
+*     /user/dados:
+*     get:
+*         summary: Pega as informações de nível e pontos do usuário
+*     responses:
+*     200:
+*         description: Sucesso
+*         content:
+*             application/json:
+*                schema:
+*                 type: array
+*                 items:
+*                  type: object
+*/
+router.get('/user/dados/', getDados);
--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
+require('dotenv').config()
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+
 const app = express();
 const PORT = 3000;
 const usersRouter = require('./routes/usersRouter')
 const loginRouter = require('./routes/loginRouter');
+
 
 // Variáveis para armazenar as contagens de respostas
 let respostas = { A: 0, B: 0, C: 0 };
@@ -13,8 +18,21 @@ let respostas = { A: 0, B: 0, C: 0 };
 // Middleware para analisar corpos de solicitações URL-encoded e JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(cors());
+
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'API de Tarefas',
+        version: '1.0.0',
+        description: 'API CRUD para gerenciar tarefas',
+      },
+      servers: [{ url: 'http://localhost:3000' }],
+    },
+    apis: [`${__dirname}/routes/*.js`], // caminho para as rotas
+  };
 
 // Redirecionar para a home ao acessar a raiz
 
@@ -28,3 +46,6 @@ app.use('/api', loginRouter)
 app.listen(PORT, () => {
     console.log(`Quiz app listening at http://localhost:${PORT}`);
 });
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
